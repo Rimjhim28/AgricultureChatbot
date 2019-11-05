6a016ddd9a6d81a184ae2ba58522dc88f69ca8ba
Rimjhim28 <rimjhimbhadani@gmail.com>
2019-11-06 02:04:00 +0530

    proper prediction

--- app.py
+++ app.py
@@ -1,7 +1,46 @@
 # Import libraries
 import numpy as np
+import pandas as pd
 from flask import Flask, request, jsonify
 import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.model_selection import train_test_split
+
+#model code
+#Importing the data
+data = pd.read_csv("data.csv", usecols = ["Crop", "QueryType", "QueryText", "KccAns"])
+#print(data.head())
+
+#PreProcess Data
+df = pd.DataFrame(columns=['Crop', 'QueryType', 'QueryText', 'KccAns'])
+for index, row in data.iterrows():
+    b = []
+    crop = row['Crop']
+    a = crop.split('(')
+    for x in a:
+        temp_list = x.split('/')
+        for crop in temp_list:
+            crop.lower()
+            b.append(crop)
+    b[:] = [s.lower() for s in b]
+    b[:] = [s.strip(')') for s in b]
+    b[:] = [s.strip() for s in b]
+    for x in b:
+        df = df.append({'Crop':x, 'QueryType': row['QueryType'], 'QueryText': row['QueryText'], 'KccAns': row['KccAns']}, ignore_index=True)
+data = df
+print(type(data))
+
+#Test-Train split
+X_train, X_test, y_train, y_test = train_test_split(data['QueryText'], data['QueryType'], test_size=0.33, random_state=53)
+
+#Count Vectorizer
+count_vectorizer = CountVectorizer(stop_words='english')
+count_train = count_vectorizer.fit_transform(X_train.values)
+
+#Tfidf Transformer
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(count_train)
 
 app = Flask(__name__)
 
@@ -11,9 +50,13 @@ model = pickle.load(open('model.pkl','rb'))
 @app.route('/api', methods=['POST'])
 def predict():
 	data = request.get_json(force=True)
-	print(np.array(data['exp']).shape)
-	prediction = model.predict(np.array(data['exp']).reshape(-1,1))
+	print('HEEELLLOOOO')
+	input = data['exp']
+	input_count = count_vectorizer.transform(input)
+	input_tfidf = tfidf_transformer.transform(input_count)
+	prediction = model.predict(input_tfidf)
 	output = prediction[0]
+	print(output)
 	return jsonify(output)
 
 if __name__ == '__main__':
--- request.py
+++ request.py
@@ -2,5 +2,5 @@ import requests
 
 url = 'http://localhost:5000/api'
 
-r = requests.post(url,json={'exp':'how to grow weed',})
+r = requests.post(url,json={'exp':['how to grow cotton']})
 print(r)
