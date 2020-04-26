6e221b92270d3d550a6572e329bc1d897d9e0f39
Sayari Bhattacharyya <47933908+Sayari253@users.noreply.github.com>
2020-04-26 16:42:59 +0530

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,9 @@
 import requests
+from googletrans import Translator
 
 url = 'http://localhost:5000/api'
-
-r = requests.post(url,json={'exp':['how to grow cotton']})
+query = 'कपास कैसे बढ़ती है'
+t = Translator().translate(query)
+new_query = t.text
+r = requests.post(url,json={'exp':[new_query]})
 print(r)
