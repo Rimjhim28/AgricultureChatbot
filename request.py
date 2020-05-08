e0d7416f8aa95c616e73ced5343828460711d155
Sayari Bhattacharyya <47933908+Sayari253@users.noreply.github.com>
2020-05-08 20:05:45 +0530

    Update request.py

--- request.py
+++ request.py
@@ -3,7 +3,5 @@ from googletrans import Translator
 
 url = 'http://localhost:5000/api'
 query = 'कपास कैसे बढ़ती है'
-t = Translator().translate(query)
-new_query = t.text
-r = requests.post(url,json={'exp':[new_query]})
+r = requests.post(url,json={'exp':[query]})
 print(r)
