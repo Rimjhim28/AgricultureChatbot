2e8460511f11159d15931c2cbaef1fc2de056cba
Rimjhim <rimjhi@amazon.com>
2020-05-10 11:52:48 +0530

    Removed unrequired files

--- AndroidApp/app/src/main/java/com/example/agriculturechatbot/BotResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.agriculturechatbot;
-
-class BotResponse {
-    String query, response;
-    BotResponse(String query, String response) {
-        this.query = query;
-        this.response = response;
-    }
-}
